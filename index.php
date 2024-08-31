bf7fa895c819ec8367ad46dc8ca6b40580289789
cortins-05 <lucasortins@gmail.com>
2024-08-31 23:46:21 +0200

    d

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Silicium</title>
+    <link rel="icon" href="../public/img/silicio.png">
+</head>
+<body>
+    
+</body>
+</html>
