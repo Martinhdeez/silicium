043b965626fe3660b81b63d243062910e8751cff
cortins-05 <lucasortins@gmail.com>
2024-09-01 01:20:11 +0200

    e

--- views/id/login.php
+++ views/id/login.php
@@ -12,14 +12,14 @@ require_once("../../layouts/header.php");
                 success();
             ?>
             <div >
-                <label for="username">Username</label>
+                <label for="username"><img id="usuario" src="../../public/img/perfil.png" alt=""></label>
                 <input type="text" id="username" placeholder="Username" name="username" value="<?php echo isset($_SESSION['form_data']['username']) ? htmlspecialchars($_SESSION['form_data']['username']) : ''; ?>" required>
             </div>
             <div>
                 <label for="password">Password</label>
                 <input type="password" id="password" placeholder="Password" name="password" required>
             </div>
-            <button type="submit" name="login_submit">Login</button>
+            <button id="button" type="submit" name="login_submit">Login</button>
             <a href="register.php">You still do not have an account?</a>
         </form>
     </div>
