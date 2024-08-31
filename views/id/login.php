cf9b057516715fdbb2eb1e4958597badf7ba9e19
cortins-05 <lucasortins@gmail.com>
2024-09-01 01:08:21 +0200

    fondo

--- views/id/login.php
+++ views/id/login.php
@@ -10,14 +10,14 @@ require_once("../../includes/functions.php");
     <link rel="stylesheet" href="../../public/css/id_styles.css">
 </head>
 <body>
-    <div>
+    <div id="background">
         <form id="loginForm" action="../../controllers/id/loginController.php" method="post" class="form">
             <h2>User Login</h2>
             <?php
                 session_start();
                 success();
             ?>
-            <div>
+            <div >
                 <label for="username">Username</label>
                 <input type="text" id="username" placeholder="Username" name="username" value="<?php echo isset($_SESSION['form_data']['username']) ? htmlspecialchars($_SESSION['form_data']['username']) : ''; ?>" required>
             </div>
