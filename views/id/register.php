86ed5d8b36e04adca66c9d17b5ef905e3e406194
martinhdeez <m.hernandezg@udc.es>
2024-09-01 00:43:32 +0200

    login user

--- views/id/register.php
+++ views/id/register.php
@@ -1,8 +1,14 @@
 <?php
-require_once("../parts/header.php");
 require_once("../../includes/functions.php");
 session_start();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
      <div>
         <form action="../controllers/registerController.php" method="post">
             <h2>User Registration</h2>
