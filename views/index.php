365d09d5b3c9ecbb5aba54c8b7b38ed01e056364
cortins-05 <lucasortins@gmail.com>
2024-09-01 00:10:11 +0200

    favicon

--- views/index.php
+++ views/index.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Silicium</title>
-    <link rel="icon" href="">
+    <link rel="icon" href="../public/img/silicio.png">
 </head>
 <body>
     
