7f7980c8483a24e8e17e774d87b73349253b030f
martinhdeez <m.hernandezg@udc.es>
2024-09-01 00:06:28 +0200

    login hecho

---
+++ views/id/login.php
@@ -0,0 +1,25 @@
+<?php
+require_once("../parts/header.php");
+require_once("../../includes/functions.php");
+?>
+    <div>
+        <form action="../controllers/LoginController.php" method="post">
+            <h2>User Login</h2>
+            <?php
+                session_start();
+                success();
+            ?>
+            <div>
+                <label for="username">Username</label>
+                <input type="text" id="username" placeholder="Username" name="username" value="<?php echo isset($_SESSION['form_data']['username']) ? htmlspecialchars($_SESSION['form_data']['username']) : ''; ?>" required>
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Password" name="password" required>
+            </div>
+            <button type="submit" name="login_submit">Login</button>
+            <a href="register.php">You still do not have an account?</a>
+        </form>
+    </div>
+</body>
+</html>
