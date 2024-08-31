69f3772a13ed360de8beb4c4ea3faffe9b327df9
martinhdeez <m.hernandezg@udc.es>
2024-08-31 20:49:13 +0200

    Primer commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Silicium</title>
+</head>
+<body>
+    
+    <h1>ESTE ES EL PROYECTO</h1>
+
+</body>
+</html>
